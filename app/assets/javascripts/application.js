6b6fd9de84b0a172476d56aeb8f6d3c701e7e6c3
Simeon Muyiwa <muyiwa100@yahoo.com>
2015-06-22 17:18:10 +0100

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require ember
+//= require ember-data
+//= require_self
+//= require ./healthy
+
+// for more details see: http://emberjs.com/guides/application/
+Healthy = Ember.Application.create();
+
---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+Healthy.Router.map(function() {
+   this.resource('hearts');
+});

29b6a57d2a3a037732df8777a82eaf9631ac8f51
Simeon Muyiwa <muyiwa100@yahoo.com>
2015-07-03 19:16:08 +0100

    update rails from 4.2 to 4.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  resources :users
-
+  
+  
   resources :hearts
 
+  resources :users
   root :to => 'assets#index'
   get 'assets/index'
+
+  get 'signup' => 'users#new'
+
+  
+
+  
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
